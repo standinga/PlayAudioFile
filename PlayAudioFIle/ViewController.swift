a7e0dbf0e1927f73a66106c8f6091c7efe23e38f
standinga <standinga@o2.pl>
2016-10-27 03:38:02 +0200

    Initial Commit

---
+++ PlayAudioFIle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayAudioFIle
+//
+//  Created by michal on 27/10/16.
+//  Copyright © 2016 borama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
